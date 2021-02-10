0755c0b17fa2f74247b66deda4045a874e229723
VladimirCharkot <vlad.chk@gmail.com>
2021-02-10 01:10:21 -0300

    Mejorando mobile

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -3,15 +3,32 @@ window.onscroll = function() { barra() };
 
 window.last_scroll_check = document.documentElement.scrollTop // comparar con esto para ver si hay gesto ascendente
 
-function barra(){
+function ajustar_cabecera() {
   let cabecera = document.querySelector('#cabecera')
-  let contenido = document.querySelector('#contenido')
-  if (document.documentElement.scrollTop < 20){
-    document.querySelector('#cabecera').classList.remove('contraida')
-  }
-  if (document.documentElement.scrollTop > 50) {
-    document.querySelector('#cabecera').classList.add('contraida')
+  console.log(window.innerWidth)
+  if (window.innerWidth <= 1000) cabecera.classList.add('contraida');
+  else cabecera.classList.remove('contraida');
+
+  document.querySelector('#contenido').style.top = (document.querySelector('#cabecera').getBoundingClientRect().height - 20) + 'px'
+};
+
+
+
+function barra(){
+  if (window.innerWidth > 1000){
+    let cabecera = document.querySelector('#cabecera')
+    let contenido = document.querySelector('#contenido')
+    
+    if (document.documentElement.scrollTop < 20){
+      document.querySelector('#cabecera').classList.remove('contraida')
+    }
+    if (document.documentElement.scrollTop > 50) {
+      document.querySelector('#cabecera').classList.add('contraida')
+    }
   }
 }
 
-window.onload = () => { document.querySelector('#contenido').style.top = (document.querySelector('#cabecera').getBoundingClientRect().height - 20) + 'px'}
+
+window.addEventListener('load', ajustar_cabecera)
+window.addEventListener('resize', ajustar_cabecera)
+window.addEventListener('scroll', barra)
