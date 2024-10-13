3f4509d206271e34737c6a7cb012300f850c8f30
VladimirCharkot <vlad.chk@gmail.com>
2024-10-12 23:40:09 -0300

    google analytics pero bien

--- front/sitio.tsx
+++ front/sitio.tsx
@@ -8,16 +8,6 @@ interface SitioProps extends PropsWithChildren{
 export const Sitio  = ({titulo, children}: SitioProps) => (
     <html>
         <head>
-
-            <script async src="https://www.googletagmanager.com/gtag/js?id=G-FLJ7JG40P4"></script>
-            <script>
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-FLJ7JG40P4');
-            </script>
-
             <title>{titulo ?? 'El Silencio Donde Escucho'}</title>
             <link rel="stylesheet" href="/css/style.css" />
             <link rel="icon" href="/icon/leaf.png" />
